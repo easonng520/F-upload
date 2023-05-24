a03993ec6f00c9922099a09047070299b96508fa
easonng520 <121944894+easonng520@users.noreply.github.com>
2023-05-24 00:51:22 +0000

    working

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://bu.easonng520.repl.co",
+  baseURL: "https://b-upload.easonng520.repl.co/files",
+// baseURL: "https://backend.easonng520.repl.co/api/files",
+  
   headers: {
     "Content-type": "application/json",
   },
